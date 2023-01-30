0fdd846c969fb3ceaaa5871396bb29584bbb5468
Victor-Guerrero-Devs <Vicguerrero.dev@gmail.com>
2023-01-30 00:01:09 -0600

    Add handle submit

--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -1,16 +1,24 @@
+import { useRef } from 'react';
 import { Form, Stack, Row, Col, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import CreatableReactSelect from 'react-select/creatable';
 
 const NoteForm = () => {
+  const titleRef = useRef<HTMLInputElement>(null);
+  const markdownRef = useRef<HTMLTextAreaElement>(null);
+
+  const handleSubmit = (e: FormEvent) {
+    e.preventDefault();
+  }
+  
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Stack gap={4}>
         <Row>
           <Col>
             <Form.Group controlId="title">
               <Form.Label>Title</Form.Label>
-              <Form.Control type="text" placeholder="Note Title" required />
+              <Form.Control type="text" ref={titleRef} required />
             </Form.Group>
           </Col>
           <Col>
@@ -22,7 +30,13 @@ const NoteForm = () => {
         </Row>
         <Form.Group controlId="markdown">
           <Form.Label>Body</Form.Label>
-          <Form.Control type="text" as="textarea" rows={15} required />
+          <Form.Control
+            type="text"
+            as="textarea"
+            rows={15}
+            ref={markdownRef}
+            required
+          />
         </Form.Group>
         <Stack direction="horizontal" gap={2} className="justify-content-end">
           <Button type="submit" variant="primary">
